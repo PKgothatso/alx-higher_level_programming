816e4a2ffa41a41ac91ae3adaa1c1a6ad9821405
PKgothatso <ksp331@outlook.com>
2024-01-24 02:05:55 -0800

    MagicClass ByteCode

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -30,7 +30,7 @@ class Square:
     def area(self):
         """Return the current area of the square"""
         return (self.__size * self.__size)
-    
+
     def __eq__(self, other):
         """Define the == comparision to a Square."""
         return self.area() == other.area()
